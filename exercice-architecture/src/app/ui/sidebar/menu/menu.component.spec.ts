d199cd27f981b47777337c5a48a30acbfa356299
ihab abadi <utopios@imac-de-ihab.home>
2021-10-11 16:11:03 +0200

    suite test, test d'un composant

--- exercice-architecture/src/app/ui/sidebar/menu/menu.component.spec.ts
+++ exercice-architecture/src/app/ui/sidebar/menu/menu.component.spec.ts
@@ -5,7 +5,7 @@ import { MenuComponent } from './menu.component';
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
-
+  let element: HTMLElement
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ MenuComponent ]
@@ -14,12 +14,34 @@ describe('MenuComponent', () => {
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MenuComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    
   });
 
   it('should create', () => {
+    fixture = TestBed.createComponent(MenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('render menuItem title', () => {
+    fixture = TestBed.createComponent(MenuComponent);
+    component = fixture.componentInstance;
+    component.menuItem = {path :'/lien', title:'lienTest'}
+    fixture.detectChanges()
+    element = fixture.nativeElement
+    const title = element.querySelector(".link-menu")?.textContent
+    expect(title).toContain('lienTest')
+  })
+
+  it('render menuItem path', () => {
+    fixture = TestBed.createComponent(MenuComponent);
+    component = fixture.componentInstance;
+    component.menuItem = {path :'/lien', title:'lienTest'}
+    fixture.detectChanges()
+    element = fixture.nativeElement
+    const path = element.querySelector(".link-menu")
+    console.log(path)
+    expect(path).toContain('/lien')
+  })
 });
