25b62480615790d6fc773a054778a08963b8e045
ihab abadi <utopios@imac-de-ihab.home>
2021-10-11 10:14:20 +0200

    suite componsant reutilisable

--- demo-app/src/app/ui/carousel/components/carousel/carousel.component.ts
+++ demo-app/src/app/ui/carousel/components/carousel/carousel.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CarouselService } from '../../carousel.service';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
+  providers:[CarouselService]
 })
 export class CarouselComponent implements OnInit {
   @Input() delay = 500
---
+++ demo-app/src/app/ui/carousel/index.ts
@@ -0,0 +1,3 @@
+export { CarouselModule } from "./carousel.module"
+export {CarouselComponent} from './components/carousel/carousel.component'
+
