60f03b0fa966ab6ad2ce948f2de31416ba1547d2
ihab abadi <utopios@imac-de-ihab.home>
2021-10-12 12:01:21 +0200

    prelaod strategy

--- routing-app/src/app/app-routing.module.ts
+++ routing-app/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NoPreloading, PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { CustomPreloadStrategyService } from './core/services/custom-preload-strategy.service';
 
 import { ContactPage } from './pages/contact.page';
 import { HomePage } from './pages/home.page';
@@ -7,14 +8,18 @@ import { HomePage } from './pages/home.page';
 
 const routes: Routes = [
   {path :'', component:HomePage},
-  {path:'auth', loadChildren : () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {path:'auth', data: {preload: true}, loadChildren : () => import('./auth/auth.module').then(m => m.AuthModule)},
   {path :'contact', component: ContactPage},
   {path :'products', loadChildren : () => import('./product/product.module').then(m => m.ProductModule)},
   {path: '**', redirectTo : ''}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    //PreloadAllModules permet de charger la totalité des modules même en lazy loading
+    //NoPreloading => par défaut
+    preloadingStrategy: CustomPreloadStrategyService
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
