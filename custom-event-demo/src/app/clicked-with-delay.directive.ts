fa403a84fe0166331ca9ec2eb647c8f6c1cb9f8c
ihab abadi <utopios@imac-de-ihab.home>
2021-10-12 15:46:24 +0200

    suite custom event angualr

--- custom-event-demo/src/app/clicked-with-delay.directive.ts
+++ custom-event-demo/src/app/clicked-with-delay.directive.ts
@@ -1,14 +1,18 @@
-import { Directive, EventEmitter, Output } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, Output, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[clickedDelay]'
 })
 export class ClickedWithDelayDirective {
   @Output('clickedDelay') clicked = new EventEmitter<string>()
-  constructor() { 
-    setTimeout(() => {
-      this.clicked.emit('Event ok')
-    }, 2000)
+  constructor(private renderer: Renderer2, private el: ElementRef) {
+    //Service renderer2 permet d'acceder au dom réel.
+    //Service elementRef permet de récupérer l'élément sur le quel la directive est appliquée
+    this.renderer.listen(this.el.nativeElement, 'click', (event) => {
+      setTimeout(() => {
+        this.clicked.emit('Event ok')
+      }, 2000)
+    })
   }
 
 }
